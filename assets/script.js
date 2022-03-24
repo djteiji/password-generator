648b47979292bfe80b03f3525411b3002f55e4df
djteiji <djteiji@gmail.com>
2022-03-24 02:32:56 -0700

    add password info and randomize functions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,62 @@
 // Assignment code here
 
-var generatePassword = function () {
+var passwordInfo = {
+  lower: "hither",
+  upper: upperCase,
+  number: numberGenerator,
+  symbol: symbolGenerator
+}
+console.log(passwordInfo.lower);
 
-  var passwordLength = prompt("What is your password length? Enter a number from 8-128.");
-  if (passwordLength < 8 || passwordLength> 128) {
-    window.alert("You need to provide a valid answer! Please try again.");
-    return generatePassword();
-  }
-    var confirmLowerCase = confirm("Would you like to include lower case letters in your password?");
-  //  if confirmLowerCase {}
+var lowerCase = function  () {
+  return String.fromCharCode(Math.floor(Math.random() *26)+ 97);
+}
 
-    var confirmUpperCase = confirm("Would you like to include upper case letters in your password?");
+var upperCase = function () {
+  return String.fromCharCode(Math.floor(Math.random() *26)+ 65);
+}
 
-    var confirmNumerical = confirm("Would you like to include numerical characters in your password?");
+var numberGenerator = function () {
+  return String.fromCharCode(Math.floor(Math.random() *10)+ 48);
+}
 
-    var confirmSpecialCharacters = confirm("Would you like to include special characters in your password?");
-  }
+var symbolGenerator = function () {
+  var symbols = "!#$%&'()*+,-./:;<=>?@[/]^_`{|}~";
+  return symbols[Math.floor(Math.random()* symbols.length)]; 
+}
+console.log(lowerCase());
+
+
+
+
+// var password =document.getElementById("password");
+
+// var passwordElements = [];
+// console.log(passwordElements);
+
+
+// var generatePassword = function () {
+
+//   var passwordLength = (8-128);
+//   console.log(passwordLength);
+  // prompt("What is your password length? Enter a number from 8-128.");
+  // if (passwordLength < 8 || passwordLength> 128) {
+  //   window.alert("You need to provide a valid answer! Please try again.");
+  //   return generatePassword();
+  // } else passwordElements.push(passwordLength);
+
+
+
+  //   var confirmLowerCase = confirm("Would you like to include lower case letters in your password?");
+  // //  if confirmLowerCase {}
+      
+
+  //   var confirmUpperCase = confirm("Would you like to include upper case letters in your password?");
+
+  //   var confirmNumerical = confirm("Would you like to include numerical characters in your password?");
+
+  //   var confirmSpecialCharacters = confirm("Would you like to include special characters in your password?");
+  // }
 
 
 
