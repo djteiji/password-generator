caec1ec2cd158110caaf46f8c113b6c8e0bd1dd5
djteiji <djteiji@gmail.com>
2022-03-25 19:58:17 -0700

    fix prompts, now all working as expected

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,11 @@ var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbolsArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", "[", "]", "{", "}", "|", "/", "?","<", ">", "."];
 
-
+var passwordInfoArr
 
 function promptUser () {
   passwordInfoArr = [];
+  
 
   passwordLength = prompt("What is your password length? Enter a number from 8-128.");
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength> 128) {
@@ -25,13 +26,13 @@ function promptUser () {
     passwordInfoArr = passwordInfoArr.concat(upperCaseArr);
   }
 
-  if (confirm("Would you like to include numerical characters in your password?")); {
+  if (confirm("Would you like to include numerical characters in your password?")) {
     passwordInfoArr = passwordInfoArr.concat(numbersArr);
   }
 
-  if (confirm("Would you like to include special characters in your password?")); {
+  if (confirm("Would you like to include special characters in your password?")) {
     passwordInfoArr = passwordInfoArr.concat(symbolsArr);
-  }
+  } 
 }
 
 
@@ -39,9 +40,9 @@ function generatePassword() {
   promptUser();
   var password = "";
   for(var i = 0; i < passwordLength; i++) {
-    var randomCharacter = Math.floor(Math.random() * passwordInfoArr.length);
-    password = password + passwordInfoArr[randomCharacter];
-  }
+    var randomCharacterIndex = Math.floor(Math.random() * passwordInfoArr.length);
+    password = password + passwordInfoArr[randomCharacterIndex];
+  } 
   return password;
 }
 
